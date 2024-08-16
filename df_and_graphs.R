0a687daf02a7a5dcf2a88b4b0cd1bc2d50fc2df2
Kelvin Chilvers-Jones <119540748+kcj-coding@users.noreply.github.com>
2024-08-16 07:48:44 +0100

    Add files via upload

--- df_and_graphs.R
+++ df_and_graphs.R
@@ -1,6 +1,8 @@
+#library(tidyverse)
 library(dplyr)
-library(summarytools)
 library(ggplot2)
+library(summarytools)
+
 
 data <- c(1,2,3,4,5,6,7,8,9,10)
 typer <- c("a","b","a","b","a","b","a","b","a","b")
@@ -113,24 +115,24 @@ data_ends <- df %>%
 
 #create line graph
 ggplot(df,aes(x=`row_nums_same`,y=`c_number`, group=`type`, color=`type`))+
-#ggplot(df,aes(x=`row_nums_same`,y=`c_number`, group=1, color=1))+  # single line only
-geom_line()+
-#scale_x_date(date_labels="%d/%m/%Y",date_breaks="1 week")+ #can be 1 day, 1 week, 1 month
-#geom_text(data=filter(df,`c_number`==last(`c_number`)), aes(label=round(`c_number`,0)),vjust=-0.5)+
-geom_text(data=data_ends,aes(label=round(`c_number`,0)),vjust=-0.5,size=2.5)+
-#geom_text(data=filter(df_c,tot == last(tot)), aes(label=round(`tot`,0)),vjust=-0.5,size=2.5)+ # single line only
-scale_color_manual(values=alpha(c("#CD2456","#14022E")),name="xyz")+
-#scale_x_continuous(1:5)+
-#geom_point(aes(shape=`column`))+
-#geom_vline(aes(xintercept=as.numeric(Date[c(90)])))+
-#geom_vline(aes(xintercept=as.integer(as.POSIXct("2020-05-05"))),linetype=4)+
-theme_classic()+
-ylim(0,max((df$`c_number`)+5))+
-#facet_grid(~type)+
-#coord_flip()+
-theme(legend.position="none")+
-theme(axis.text.x=element_text(angle=0,size=10))+
-labs(x="Instance",y="Cumulative",title=grptt)
+  #ggplot(df,aes(x=`row_nums_same`,y=`c_number`, group=1, color=1))+  # single line only
+  geom_line()+
+  #scale_x_date(date_labels="%d/%m/%Y",date_breaks="1 week")+ #can be 1 day, 1 week, 1 month
+  #geom_text(data=filter(df,`c_number`==last(`c_number`)), aes(label=round(`c_number`,0)),vjust=-0.5)+
+  geom_text(data=data_ends,aes(label=round(`c_number`,0)),vjust=-0.5,size=2.5)+
+  #geom_text(data=filter(df_c,tot == last(tot)), aes(label=round(`tot`,0)),vjust=-0.5,size=2.5)+ # single line only
+  scale_color_manual(values=alpha(c("#CD2456","#14022E")),name="xyz")+
+  #scale_x_continuous(1:5)+
+  #geom_point(aes(shape=`column`))+
+  #geom_vline(aes(xintercept=as.numeric(Date[c(90)])))+
+  #geom_vline(aes(xintercept=as.integer(as.POSIXct("2020-05-05"))),linetype=4)+
+  theme_classic()+
+  ylim(0,max((df$`c_number`)+5))+
+  #facet_grid(~type)+
+  #coord_flip()+
+  theme(legend.position="none")+
+  theme(axis.text.x=element_text(angle=0,size=10))+
+  labs(x="Instance",y="Cumulative",title=grptt)
 
 
 #savename of graphs
@@ -140,16 +142,70 @@ labs(x="Instance",y="Cumulative",title=grptt)
 #ggsave(path="C:\\",svname,height=15,width=30,units="cm")
 
 ################################################################################
+# line graph with only 1 line coloured
+# https://stackoverflow.com/questions/68079185/how-to-make-one-line-color-in-geom-line-to-overshadow-other-line-colors
+# https://stackoverflow.com/questions/72058812/highlighting-lines-and-gray-out-rest-in-multiple-line-chart-with-ggplot2
+
+for (i in unique(df$type)){
+  
+  #filter for last value
+  data_ends <- df %>%
+    filter(type == i) %>%
+    group_by(`type`) %>%
+    top_n(1,`c_number`)
+  
+  #savename of graph title
+  grptt=paste("title",sep="")
+  
+  # set colours of lines
+  #line_clr <- c("#CD2456", "#CECECE")
+  
+  #create line graph
+  ggplot(df,aes(x=`row_nums_same`,y=`c_number`, group=`type`, color=ifelse(type==i, "#CD2456","#CECECE")))+#`type`))+
+    #ggplot(df,aes(x=`row_nums_same`,y=`c_number`, group=1, color=1))+  # single line only
+    geom_line(aes(group=type, color=ifelse(type==i, "#CD2456","#CECECE")))+#`type`))+
+    # add line of interest
+    #geom_line(data=df_alt[df_alt$type == i,], aes(group=type, color=type))+ # filter line to colour as line of interest
+    #scale_x_date(date_labels="%d/%m/%Y",date_breaks="1 week")+ #can be 1 day, 1 week, 1 month
+    #geom_text(data=filter(df,`c_number`==last(`c_number`)), aes(label=round(`c_number`,0)),vjust=-0.5)+
+    geom_text(data=data_ends,aes(label=round(`c_number`,0)),vjust=-0.5,size=2.5)+
+    #geom_text(data=filter(df_c,tot == last(tot)), aes(label=round(`tot`,0)),vjust=-0.5,size=2.5)+ # single line only
+    #scale_color_manual(values=alpha(c("#CD2456","#CECECE")),name="xyz")+
+    
+    scale_color_manual(values = c(rep(c("#CD2456"), each = 1),
+                                  rep("#CECECE", nrow(df) - 1)))+
+    
+    #scale_x_continuous(1:5)+
+    #geom_point(aes(shape=`column`))+
+    #geom_vline(aes(xintercept=as.numeric(Date[c(90)])))+
+    #geom_vline(aes(xintercept=as.integer(as.POSIXct("2020-05-05"))),linetype=4)+
+    theme_classic()+
+    ylim(0,max((df$`c_number`)+5))+
+    #facet_grid(~type)+
+    #coord_flip()+
+    theme(legend.position="none")+
+    theme(axis.text.x=element_text(angle=0,size=10))+
+    labs(x="Instance",y="Cumulative",title=paste(grptt,i, sep=" "))
+  
+  
+  #savename of graphs
+  svname=paste("name",i,".png",sep="")
+  
+  #save graph
+  #ggsave(path="C:\\Users\\kelvi\\Desktop\\",svname,height=15,width=30,units="cm")
+}
+
+################################################################################
 
 # make a heatmap
 ggplot(df,aes(x=`row_num`,y=`row_num`))+
-geom_tile(aes(fill=`c_number`))+
-geom_text(data=df,aes(label=round(`c_number`,0)),vjust=0.25,size=2.5)+
-#scale_x_continuous(1:10)+
-#scale_y_continuous(1:10)+
-scale_fill_distiller(palette = "RdYlBu")+
-theme_classic()+
-labs(x="Instance",y="Instance",title="title")
+  geom_tile(aes(fill=`c_number`))+
+  geom_text(data=df,aes(label=round(`c_number`,0)),vjust=0.25,size=2.5)+
+  #scale_x_continuous(1:10)+
+  #scale_y_continuous(1:10)+
+  scale_fill_distiller(palette = "RdYlBu")+
+  theme_classic()+
+  labs(x="Instance",y="Instance",title="title")
 
 #savename of graphs
 #svname=paste("name",".png",sep="")
@@ -170,3 +226,60 @@ ggplot(df,aes(x=type,y=data))+
 
 stats <- boxplot(data ~ type, data=df)
 stats
+
+####################### dates ##################################################
+# create dates from 25th february to 10th march in year 2023
+date <- as.Date("2023-02-24")#+5  for next 5 days # make one value before what is wanted
+# or can do 
+# date <- seq.Date(as.Date("2023-02-25"), length=2, by='5 days' ) # or by '3 months', '-1 year') etc
+
+end_date <- as.Date("2023-03-10")
+
+t <- difftime(end_date, date, units = c("days"))
+
+# make df of dates
+# make list of dates between these values
+dater <- data.frame(date=NULL)
+for (i in 1:difftime(end_date ,date , units = c("days"))){
+  datei <- data_frame(date=date+i)
+  dater <- rbind(dater,datei)
+}
+
+# filter to dates greater than the 3rd of march
+dater_filtered <- dater %>%
+  filter(date >= "2023-03-03")
+
+####################### times ##################################################
+# or can make df of times
+# difftime can do years, months, days, hours, minutes, secs
+
+# make a df of time between 00:00 and 23:00 (every hour)
+#start_time <- as.POSIXct("00:00")
+  
+# https://stackoverflow.com/questions/12649641/calculating-time-difference-in-r
+# https://stackoverflow.com/questions/11666172/calculating-number-of-days-between-2-columns-of-dates-in-data-frame
+# https://stackoverflow.com/questions/6871016/adding-days-to-a-date-in-python  
+# https://stackoverflow.com/questions/10322035/r-adding-days-to-a-date
+
+####################### datetime ##################################################
+start <- as.POSIXct("2023-02-24 00:00:00")
+# add 3 hours
+end <- start + 3*60*60
+
+# make df of these entries
+hoursr <- data.frame(date=NULL)
+for (i in 1:difftime(end ,start , units = c("hours"))){
+  hoursi <- data_frame(date=start+(i*60*60))
+  hoursr <- rbind(hoursr,hoursi)
+}
+
+##################################################################################
+
+# datetime graphs
+dater$num <- 1
+
+# graph of dates with 3 days separation
+ggplot(dater,aes(date,num))+
+  geom_point()+
+  scale_x_date(date_labels="%d-%m",date_breaks  ="3 days") # days, months, years
+
