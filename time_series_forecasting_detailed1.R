5545f400795a4cf9e238f7e3ee2c2fc8bb92e205
Kelvin Chilvers-Jones <119540748+kcj-coding@users.noreply.github.com>
2024-09-04 06:48:36 +0100

    Add files via upload

--- time_series_forecasting_detailed1.R
+++ time_series_forecasting_detailed1.R
@@ -3,6 +3,10 @@ library(fpp3)
 # https://otexts.com/fpp3/
 library(ggplot2)
 
+output_folder <- "C:\\Users\\kelvi\\Desktop\\"
+
+graph_title = "Number by day UK"
+
 # get data in timeseries format - month year, date etc
 
 # create some dates
@@ -12,6 +16,12 @@ some_dates <- data.frame(dates=seq(as.Date("2014-03-09"), by = "day", length.out
 # note that for fpp3 package dates have to be in a certain format https://otexts.com/fpp3/tsibbles.html
 
 # and the index has to be the same object
+
+########
+# also note that for ranges e.g. months, dates, they should be sequential and if not existing for a day/month replace with 0 for true time picture
+
+# e.g. create range of dates and then left_join data on these dates, and fill blank/NA with 0
+#######
 
 # convert to dates
 dates <- data.frame(dates=format(as.Date(some_dates$dates),format="%b-%Y"))
@@ -42,23 +52,28 @@ print(df_eng)
 # view a plot of the timeseries data
 ggplot(df_eng, aes(x=Day, y=Data))+
   geom_line()+
-  labs(x="Day", y="Number", title="Number by Day for England")
+  labs(x="Day", y="Number", title=graph_title)
+ggsave(paste(output_folder,"basic_graph1.png",sep=""),width=30,height=15,units="cm")
 
+#dev.print(png,paste(output_folder,"chart.png",sep=""),width=1920,height=1080)
 autoplot(df_eng,Data) +
-  labs(title = "Number by Day for England",
+  labs(title = graph_title,
        y="Number")
+ggsave(paste(output_folder,"basic_graph2.png",sep=""),width=30,height=15,units="cm")
 
 # check for differences, make sure stationary
 df_eng %>%
   gg_tsdisplay(difference(Data, 1),
                plot_type='partial', lag=5) +
   labs(title="Seasonally differenced", y="")
+ggsave(paste(output_folder,"differences1.png",sep=""),width=30,height=15,units="cm")
 
 # difference again
 df_eng %>%
   gg_tsdisplay(difference(Data, 1) |> difference(),
                plot_type='partial', lag=5) +
   labs(title = "Double differenced", y="")
+ggsave(paste(output_folder,"differences2.png",sep=""),width=30,height=15,units="cm")
 
 # model the data with different model types and pick best fit
 
@@ -88,11 +103,13 @@ augment(fit) %>%
 
 # forecast future values
 
+
 forecast(fit, h=36) %>%
   filter(.model=='arima210011') %>%
   autoplot(df_eng) +
-  labs(title = "Number by Day for England",
+  labs(title = paste(graph_title,", ARIMA model", sep=""),
        y="Number")
+ggsave(paste(output_folder,"forecast.png",sep=""),width=30,height=15,units="cm")
 
 ################################################################################
 
@@ -111,8 +128,9 @@ df_stl %>%
   autoplot(.sim) +
   autolayer(df_eng, Data) +
   guides(colour = "none") +
-  labs(title = "Number by Day for England",
+  labs(title = graph_title,
        y="Number")
+ggsave(paste(output_folder,"bootstrap_generate.png",sep=""),width=30,height=15,units="cm")
 
 sim <- df_stl %>%
   generate(new_data = df_eng, times = 100,
@@ -128,8 +146,9 @@ ets_forecasts %>%
   autoplot(.mean) +
   autolayer(df_eng, Data) +
   guides(colour = "none") +
-  labs(title = "Number by Day for England: bootstrapped forecasts",
+  labs(title = paste(graph_title,": bootstrapped forecasts", sep=""),
        y="Number")
+ggsave(paste(output_folder,"bootstrap_forecast.png",sep=""),width=30,height=15,units="cm")
 
 bagged <- ets_forecasts %>%
   summarise(bagged_mean = mean(.mean))
@@ -139,5 +158,6 @@ df_eng %>%
   forecast(h = 12) %>%
   autoplot(df_eng) +
   autolayer(bagged, bagged_mean, col = "#D55E00") +
-  labs(title = "Number by Day for England Forecasts",
+  labs(title = paste(graph_title,", ETS model from bootstrapped averages", sep=""),
        y="Number")
+ggsave(paste(output_folder,"bagged_bootstrap_forecast.png",sep=""),width=30,height=15,units="cm")
