de2a627877419476bf13fa060d810bdb6a0ea3a8
Kelvin Chilvers-Jones <119540748+kcj-coding@users.noreply.github.com>
2024-11-03 20:11:09 +0000

    Add files via upload

---
+++ text_scrambler.R
@@ -0,0 +1,35 @@
+library(dplyr)
+
+text <- "The day was nice. 7,8,94"
+
+letters <- c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+numbers <- "0123456789"
+
+# for each word in text, replace each letter by a random letter from letters
+phrase <- NULL
+for (word in 1:length(unlist((strsplit(text, " "))))){
+  word1 <- unlist((strsplit(text, " ")))[word]
+  new_word <- NULL
+    for (i in seq(1,nchar(word1),1)){
+    if (grepl("([[:punct:]])+", substr(word1,i,i))){
+      new <- substr(word1,i,i)
+    }
+      else if (grepl("([[:digit:]])+", substr(word1,i,i))){
+        new <- floor(runif(1,min=0,max=9))
+      }
+      else {
+        new_letter_num <- floor(runif(1,min=1, max=length(letters)))
+        new <- letters[new_letter_num]
+        if (grepl("([[:upper:]])", substr(word1,i,i))){
+          new <- toupper(new)
+        }
+      }
+      new_word <- paste(as.character(append(new_word, paste(new, collapse = '', sep=""))),collapse='',sep="")
+      new_word1 <- as.character(new)
+  }
+  phrase <- append(phrase, paste(new_word, " ", collaspe='', sep=""))
+  phrase <- paste(as.character(phrase),collapse='',sep="")
+}
+
+print(paste("original phrase: ", text, sep=""))
+print(paste("scrambled phrase: ", phrase, sep=""))
