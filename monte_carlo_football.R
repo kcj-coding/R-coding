c7daadcd29facb1907a7a8eeb05c8203f2e67054
Kelvin Chilvers-Jones <119540748+kcj-coding@users.noreply.github.com>
2025-06-05 21:15:32 +0100

    Add files via upload

--- monte_carlo_football.R
+++ monte_carlo_football.R
@@ -23,7 +23,7 @@ loss_chg <- c(-0.1) # losing increases probability of losing again
 
 # teams play all teams twice except themselves
 
-n_runs <- 500
+n_runs <- 50
 
 goals <- c(0,1,2,3,4,5,6)
 goals_prob <- c(0.5,0.4,0.3,0.2,0.1,0.03,0.01) # unlikely to score high number of goals
@@ -32,6 +32,23 @@ goals_prob <- c(0.5,0.4,0.3,0.2,0.1,0.03,0.01) # unlikely to score high number o
 
 start_time <- Sys.time()
 
+matches <- data.frame()
+# make random playing order
+for (h in 1:length(teams)){
+  for (a in 1:length(teams)){
+    if (teams[h] != teams[a]){
+      match <- data.frame(home=teams[h], away=teams[a])
+      matches <- rbind(matches,match)
+    }}}
+
+# shuffle this ordering
+# pick random number in order and make new df
+# make sure to capture all values and not to repeat any
+
+new_order <- sample(1:nrow(matches),replace=FALSE)
+
+matches_new <- matches[new_order,]
+
 res <- data.frame()
 
 #for (run in seq(1,n_runs,1)){
@@ -39,18 +56,27 @@ res <- data.frame()
 #    for (a in seq(1,length(teams),1)){
 
 for (run in 1:n_runs){
-  for (h in 1:length(teams)){
-  for (a in 1:length(teams)){
-  if (teams[h] != teams[a]){
+  team_probs1 <- team_probs
+  for (i in 1:nrow(matches_new)){
+  #for (h in 1:length(teams)){
+  #for (a in 1:length(teams)){
+  #if (teams[h] != teams[a]){
     
     #print(teams[h]," v ", teams[a])
     #print("Home: ",teams[h], "Away: ",teams[a])
     
     # get result probability - create custom probabilities from defined starting points
     
-    h_team_prob <- team_probs[h] * play_prob[1]
+    h_team <- matches_new$home[i]
+    a_team <- matches_new$away[i]
+    
+    # find these entries in team_prob and update
+    h_loc <- which(sapply(teams, function(y) h_team %in% y))
+    a_loc <- which(sapply(teams, function(y) a_team %in% y))
+    
+    h_team_prob <- team_probs1[h_loc] * play_prob[1]
     #outcome_prob_h = [x*h_team_prob for x in outcome_prob]
-    a_team_prob <- team_probs[a] * play_prob[2]
+    a_team_prob <- team_probs1[a_loc] * play_prob[2]
     
     outcome_prob_h <- c(h_team_prob,max(h_team_prob*0.2, a_team_prob*0.2),a_team_prob)
     
@@ -62,16 +88,16 @@ for (run in 1:n_runs){
       h_score = min(6, a_score+sample(goals[2:7], size=1, replace=FALSE, prob=goals_prob[2:7]))
       # team_w prob increases
       # team_a prob decreases
-      team_probs[h] <- min(0.95,(1+win_chg[1]) * team_probs[h])
-      team_probs[a] <- max(0.01,(1+loss_chg[1]) * team_probs[a])
+      team_probs1[h_loc] <- min(0.95,(1+win_chg[1]) * team_probs[h_loc])
+      team_probs1[a_loc] <- max(0.01,(1+loss_chg[1]) * team_probs[a_loc])
     }
     else if (result == "L"){
       a_score = sample(goals[2:7], size=1, replace=FALSE, prob=goals_prob[2:7])
       h_score = max(0, a_score-sample(goals[1:6], size=1, replace=FALSE, prob=goals_prob[1:6]))
       # team_w prob decreases
       # team_a prob increases
-      team_probs[h] <- max(0.01,(1+loss_chg[1]) * team_probs[h])
-      team_probs[a] <- min(0.95,(1+win_chg[1]) * team_probs[a])
+      team_probs1[h_loc] <- max(0.01,(1+loss_chg[1]) * team_probs[h_loc])
+      team_probs1[a_loc] <- min(0.95,(1+win_chg[1]) * team_probs[a_loc])
     }
     else{
       a_score <- sample(goals[1:5], size=1, replace=FALSE, prob=goals_prob[1:5])
@@ -86,13 +112,13 @@ for (run in 1:n_runs){
     #print(result)
     
     # outcome probability
-    dff <- data.frame(run=run, home=teams[h], away=teams[a], probs=paste(paste(outcome_prob_h,sep=" "),collapse=''), result=result, score=paste(h_score,":",a_score,sep=""),goals_h=h_score, goals_a=a_score, exp_result=exp_result,
-                      h_prob=team_probs[h], a_prob=team_probs[a], h_points=h_points, a_points=a_points)
+    dff <- data.frame(run=run, home=teams[h_loc], away=teams[a_loc], probs=paste(paste(outcome_prob_h,sep=" "),collapse=''), result=result, score=paste(h_score,":",a_score,sep=""),goals_h=h_score, goals_a=a_score, exp_result=exp_result,
+                      h_prob=team_probs1[h_loc], a_prob=team_probs1[a_loc], h_points=h_points, a_points=a_points)
     
     res <- rbind(res,dff)
     
     
-  }}}}
+  }}#}}
 
 # at the end, want to track team positions by run
 df_teams <- data.frame()
@@ -210,6 +236,7 @@ ggplot(df_teams_f1, aes(x=rank, y=team, fill=pct))+
 
 #save graph
 #ggsave(path="C:\\","file.png",height=15,width=30,units="cm")
+
 
 end_time <- Sys.time()
 print(paste("runtime: ", end_time-start_time, sep=""))
